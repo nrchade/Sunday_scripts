847c09eeb42d9c8ce14d8095004cff3bea90d345
NickC <nrchade@yahoo.com>
2015-04-19 13:35:53 -0500

    Add line to read in data

--- code/gapminderexplore.R
+++ code/gapminderexplore.R
@@ -1,3 +1,5 @@
 # This is a script to explore gapminder dataset in R
 # Written by Me
 #me@me.com 4.19.15
+
+gap.in <- read.table('C:/Users/Nicholaus.Chadeayne/Desktop/swc_arlington/output/combined_gapMinder.tsv', sep = '\t')
