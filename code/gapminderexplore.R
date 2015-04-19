e80dc79ddff9dcc547f0f96845681bdc7f74f08f
NickC <nrchade@yahoo.com>
2015-04-19 14:01:10 -0500

    Added header argument

--- code/gapminderexplore.R
+++ code/gapminderexplore.R
@@ -2,4 +2,4 @@
 # Written by Me
 #me@me.com 4.19.15
 
-gap.in <- read.table('C:/Users/Nicholaus.Chadeayne/Desktop/swc_arlington/output/combined_gapMinder.tsv', sep = '\t')
+gap.in <- read.table('C:/Users/Nicholaus.Chadeayne/Desktop/swc_arlington/output/combined_gapMinder.tsv', sep = '\t', header = TRUE)
